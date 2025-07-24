03f3c0189fb9009705711b627bfa39b97eb495e5
Gabriel Paz <gabriel.paz.190@gmail.com>
2025-07-24 02:26:19 -0600

    Refactor encryption and decryption logic; replace arrays with a Map for better readability and maintainability

--- javascript/index.js
+++ javascript/index.js
@@ -1,76 +1,105 @@
-let keys = ["e", "i", "a", "o", "u"];
-let words = ["enter", "imes", "ai", "ober", "ufat"];
+const map = new Map();
 
-const advert = document.getElementById("fd");
-const cont = document.getElementById("rc");
-const copyBtn = document.getElementById("copy-text");
-let output = document.getElementById("message-des");
+map.set('e', 'enter');
+map.set('i', 'imes');
+map.set('a', 'ai');
+map.set('o', 'ober');
+map.set('u', 'ufat');
 
-const tags = [advert, cont, copyBtn, output];
+const advert = document.getElementById('fd');
+const cont = document.getElementById('rc');
+const copyBtn = document.getElementById('copy-text');
+let output = document.getElementById('message-des');
+let input = document.getElementById('text-box').value;
 
+const findKey = (value) => {
+  for (let [key, val] of map) {
+    if (val === value) {
+      return key;
+    }
+  }
+  return value;
+};
 
-function restaurarCSS (advert, cont, copyButton, output) {
-	advert.style.display ="block";
-	output.style.display = "none";
-	copyButton.style.display = "none";
-	cont.style.justifyContent = "center";
-	cont.style.height = "100%";
-}
-function cambiarCSS(advert, cont, copyButton, output){
-	advert.style.display ="none";
-	output.style.display = "block";
-	copyButton.style.display = "block";
-	cont.style.justifyContent = "space-between";
-	cont.style.height = "100%";
-	copyButton.innerHTML = "Copiar";
-}
-function validar (tags, n) {
- 	let input = document.getElementById("text-box").value;
-	const s = /\s/g;
- 	const regex = /[a-z]/g;
- 	let arr = Array.from(input, (index) => index == index.match(regex) || index == index.match(s) ? index: null);
-	if (arr.join("") === input && arr.length > 0) {
-		switch(n) {
-		case 1:
-			encrypt(tags, keys, words);	
-		break;
-		case 2:
-			decrypt(tags, keys, words);
-		break;	
-		}
-	}
-	else {
-		restaurarCSS(tags[0], tags[1], tags[2] ,tags[3]);
-	}
-}			
-function encrypt(tags, keys, words) {
-	let input = document.getElementById("text-box").value;
-	let newMessage = input
-	.replaceAll(keys[0], words[0])
-	.replaceAll(keys[1], words[1])
-	.replaceAll(keys[2], words[2])
-	.replaceAll(keys[3], words[3])
-	.replaceAll(keys[4], words[4]);
-	cambiarCSS(tags[0], tags[1], tags[2], tags[3]);
-	tags[3].innerHTML = newMessage; //Mostrará mensaje encriptado.
-}
-function decrypt(tags, keys, words) {
-	let input = document.getElementById("text-box").value;
-	let newMessage = input
-	.replaceAll(words[0], keys[0])
-	.replaceAll(words[1] , keys[1])
-	.replaceAll(words[2] , keys[2])
-	.replaceAll(words[3] , keys[3])
-	.replaceAll(words[4] , keys[4]);
-	cambiarCSS(tags[0], tags[1], tags[2], tags[3]);
-	tags[3].innerHTML = newMessage; //Mostrará mensaje desencriptado.
-}	
-function copyText(tags) {
-	let texto = tags[3].innerText;
-	navigator.clipboard.writeText(texto);
-	tags[2].innerHTML = "Texto copiado";
-}
-function cleanTextArea(tags) {
-	let input = document.getElementById("text-box");
-	input.value !=null ? input.value = null : null;
-}
+const resetCSS = () => {
+  advert.style.display = 'block';
+  output.style.display = 'none';
+  copyBtn.style.display = 'none';
+  cont.style.justifyContent = 'center';
+};
+
+const changeCSS = () => {
+  advert.style.display = 'none';
+  output.style.display = 'block';
+  copyBtn.style.display = 'block';
+  cont.style.justifyContent = 'space-between';
+  copyBtn.innerHTML = 'Copiar';
+};
+
+/**
+ * Validates the input from the element with id 'text-box'.
+ * Checks if the input contains only lowercase letters and spaces, and is not empty.
+ *
+ * @returns {boolean} True if the input is valid, false otherwise.
+ */
+const validateInput = () => {
+  const regex = /^[a-z\s]+$/;
+  return regex.test(input) && input.length > 0;
+};
+
+const encrypt = () => {
+  const isValidInput = validateInput();
+  isValidInput ? encryptMessage() : resetCSS();
+};
+const decrypt = () => {
+  const isValidInput = validateInput();
+  isValidInput ? decryptMessage() : resetCSS();
+};
+
+/**
+ * Encrypts the message entered in the input element with id 'text-box'
+ * by replacing vowels with their mapped values from the 'map' object.
+ * Updates the output element with the encrypted message and applies CSS changes.
+ *
+ * @function
+ * @returns {void}
+ */
+const encryptMessage = () => {
+  let newMessage = input
+    .replaceAll('e', map.get('e'))
+    .replaceAll('i', map.get('i'))
+    .replaceAll('a', map.get('a'))
+    .replaceAll('o', map.get('o'))
+    .replaceAll('u', map.get('u'));
+  changeCSS();
+  output.innerHTML = newMessage; //Mostrará mensaje encriptado.
+};
+
+/**
+ * Decrypts the message from the input text box by replacing encrypted substrings
+ * with their corresponding decrypted values using the findKey function.
+ * Updates the output element with the decrypted message and applies CSS changes.
+ *
+ * @function
+ * @returns {void}
+ */
+const decryptMessage = () => {
+  let newMessage = input
+    .replaceAll('enter', findKey('enter'))
+    .replaceAll('imes', findKey('imes'))
+    .replaceAll('ai', findKey('ai'))
+    .replaceAll('ober', findKey('ober'))
+    .replaceAll('ufat', findKey('ufat'));
+  changeCSS();
+  output.innerHTML = newMessage; //Mostrará mensaje desencriptado.
+};
+
+const copyText = () => {
+  navigator.clipboard.writeText(output.innerHTML);
+  copyBtn.innerHTML = 'Texto copiado';
+};
+
+const clearInput = () => {
+  let input = document.getElementById('text-box');
+  input != null ? (input.value = null) : null;
+};
